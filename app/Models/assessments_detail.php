d9226aadb94d3e90073ab2a3c304ac74b32e7cfb
cintarmynti <ramayanticinta@gmail.com>
2025-02-18 11:09:04 +0700

    setup template admin

--- app/Models/assessments_detail.php
+++ app/Models/assessments_detail.php
@@ -9,19 +9,19 @@ class assessments_detail extends Model
      protected $table = 'assessment_details';
 
      protected $fillable = [
-         'assessment_id',
-         'aspect_id',
-         'score',
-         'remarks',
+        'assessment_id',
+        'aspect_id',
+        'score',
+        'remarks',
      ];
 
-     public function assessment()
-     {
-         return $this->belongsTo(assesment::class, 'assessment_id');
-     }
+    public function assessment()
+    {
+        return $this->belongsTo(assesment::class, 'assessment_id');
+    }
 
-     public function aspect()
-     {
-         return $this->belongsTo(assesment_aspect::class, 'aspect_id');
-     }
+    public function aspect()
+    {
+        return $this->belongsTo(assesment_aspect::class, 'aspect_id');
+    }
 }
