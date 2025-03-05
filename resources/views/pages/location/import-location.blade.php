d9aea76512973044e99c192b87247e8b833c2832
cintarmynti <ramayanticinta@gmail.com>
2025-03-05 11:58:36 +0700

    memperbaiki wording

--- resources/views/pages/location/import-location.blade.php
+++ resources/views/pages/location/import-location.blade.php
@@ -4,7 +4,8 @@
     <div class="card">
         <div class="card-body">
             <h5 class="card-title">Import Location</h5>
-            <p class="card-description">Silakan unggah file location yang ingin diimport <a href="{{ asset('assets/templates/location_format.xlsx') }}" class="">Download Format</a>.</p>
+            <p class="card-description">Silakan unggah file location yang ingin diimport <br>
+                contoh format file : <a href="{{ asset('assets/templates/location_format.xlsx') }}" class="">Download Format</a>.</p>
 
 
             @if (session('success'))
