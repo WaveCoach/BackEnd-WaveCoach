49e408a9472b832b1f72c7431020dd2555bd693c
cintarmynti <ramayanticinta@gmail.com>
2025-03-05 12:11:54 +0700

    fix penamaan

--- database/seeders/InventoryManagementSeeder.php
+++ database/seeders/InventoryManagementSeeder.php
@@ -2,8 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\inventory_management;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\InventoryManagement;
 use Illuminate\Database\Seeder;
 
 class InventoryManagementSeeder extends Seeder
@@ -20,7 +19,7 @@ class InventoryManagementSeeder extends Seeder
         ];
 
         foreach ($data as $item) {
-            inventory_management::create($item);
+            InventoryManagement::create($item);
         }
     }
 }
