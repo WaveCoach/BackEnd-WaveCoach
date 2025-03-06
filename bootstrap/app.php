ef9d4fbfef0a63b1e69bdc64d6425b62e3c7f455
cintarmynti <ramayanticinta@gmail.com>
2025-03-06 13:17:27 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(EnsureFrontendRequestsAreStateful::class);
+        // $middleware->append(EnsureFrontendRequestsAreStateful::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
