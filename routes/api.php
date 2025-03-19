fa405da4985d03d0e41b971200e0b6a8fca85602
cintarmynti <ramayanticinta@gmail.com>
2025-03-19 11:20:57 +0700

    ubah agar list nya bisa diakses tanpa login

--- routes/api.php
+++ routes/api.php
@@ -15,12 +15,13 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
 Route::post('/reset-password', [AuthController::class, 'resetPassword']);
+Route::get('/list-admin', [AuthController::class, 'listAdmin']);
+
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('profile', [AuthController::class, 'profile']);
     Route::post('logout', [AuthController::class, 'logout']);
-    Route::post('/change-password', [AuthController::class, 'changePassword']);
-    Route::get('list-admin', [AuthController::class, 'listAdmin']);
+    Route::post('change-password', [AuthController::class, 'changePassword']);
 
     Route::post('absensi-coach', [AbsensiController::class, 'coachAbsent']);
     Route::post('absensi-student', [AbsensiController::class, 'studentAbsent']);
