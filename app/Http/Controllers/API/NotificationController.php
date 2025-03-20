bf7a1a54c8970d6979dc4c1025efc1385e0c0497
cintarmynti <ramayanticinta@gmail.com>
2025-03-20 11:05:24 +0700

    cek idnya ada gak

--- app/Http/Controllers/API/NotificationController.php
+++ app/Http/Controllers/API/NotificationController.php
@@ -23,29 +23,36 @@ class NotificationController extends BaseController
         $notifications = Notification::with(['user', 'pengirim'])
             ->where('user_id', $userId)
             ->orderBy('created_at', 'desc')
-            ->get()
-            ->map(function ($notif) {
-                return [
-                    'id' => $notif -> id,
-                    'notifiable_type' => $notif->notifiable_type,
-                    'title' => $notif->title,
-                    'message' => $notif->message,
-                    'is_read' => $notif->is_read,
-                    'type' => $notif->type,
-                    'created_at' => Carbon::parse($notif->created_at)->translatedFormat('d F Y, H:i'),
-                    'user' => [
-                        'id' => $notif->user->id,
-                        'name' => $notif->user->name,
-                    ],
-                    'pengirim' => [
-                        'id' => $notif->pengirim->id,
-                        'name' => $notif->pengirim->name,
-                    ],
-                ];
-            });
+            ->get();
 
-        return $this->SuccessResponse($notifications, 'Daftar notifikasi');
+        // Cek apakah ada notifikasi untuk user tersebut
+        if ($notifications->isEmpty()) {
+            return $this->ErrorResponse('Notifikasi tidak ditemukan', 404);
+        }
+
+        $formattedNotifications = $notifications->map(function ($notif) {
+            return [
+                'id' => $notif->id,
+                'notifiable_type' => $notif->notifiable_type,
+                'title' => $notif->title,
+                'message' => $notif->message,
+                'is_read' => $notif->is_read,
+                'type' => $notif->type,
+                'created_at' => Carbon::parse($notif->created_at)->translatedFormat('d F Y, H:i'),
+                'user' => [
+                    'id' => $notif->user->id,
+                    'name' => $notif->user->name,
+                ],
+                'pengirim' => [
+                    'id' => $notif->pengirim->id,
+                    'name' => $notif->pengirim->name,
+                ],
+            ];
+        });
+
+        return $this->SuccessResponse($formattedNotifications, 'Daftar notifikasi');
     }
+
 
     public function getDetailNotif($NotifId)
     {
