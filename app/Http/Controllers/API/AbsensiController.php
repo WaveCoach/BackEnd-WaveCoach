39d4e60b9a956e1b7ee9bee381b2e7117ad855a3
cintarmynti <ramayanticinta@gmail.com>
2025-03-26 13:58:45 +0700

    Update AbsensiController.php

--- app/Http/Controllers/API/AbsensiController.php
+++ app/Http/Controllers/API/AbsensiController.php
@@ -48,27 +48,42 @@ class AbsensiController extends BaseController
     public function studentAbsent(Request $request)
     {
         $validated = $request->validate([
-            'attendance_status' => 'required|string|in:Hadir,Tidak Hadir',
-            'student_id' => 'required|exists:users,id',
+            'student_attendance' => 'required|array',
+            'student_attendance.*.student_id' => 'required|exists:users,id',
+            'student_attendance.*.attendance_status' => 'required|string|in:Hadir,Tidak Hadir',
             'schedule_id' => 'required|exists:schedules,id',
         ]);
 
-        // Cek apakah absensi sudah ada
-        $existingAttendance = StudentAttendance::where('student_id', $request->student_id)
-            ->where('schedule_id', $request->schedule_id)
-            ->exists();
+        $successfulAbsences = [];
+        $failedAbsences = [];
 
-        if ($existingAttendance) {
-            return $this->ErrorResponse('Siswa ini sudah melakukan absensi untuk jadwal ini!', 400);
+        foreach ($validated['student_attendance'] as $attendance) {
+            $studentId = $attendance['student_id'];
+            $attendanceStatus = $attendance['attendance_status'];
+
+            $existingAttendance = StudentAttendance::where('student_id', $studentId)
+                ->where('schedule_id', $validated['schedule_id'])
+                ->exists();
+
+            if ($existingAttendance) {
+                $failedAbsences[] = $studentId;
+                continue;
+            }
+
+            $newAttendance = StudentAttendance::create([
+                'student_id' => $studentId,
+                'attendance_status' => $attendanceStatus,
+                'schedule_id' => $validated['schedule_id']
+            ]);
+
+            $successfulAbsences[] = $newAttendance;
         }
 
-        $attendance = StudentAttendance::create([
-            'student_id' => $request->student_id,
-            'attendance_status' => $validated['attendance_status'],
-            'schedule_id' => $request->schedule_id
-        ]);
-
-        return $this->SuccessResponse($attendance, 'Absensi berhasil disimpan', 201);
+        return $this->SuccessResponse([
+            'success' => $successfulAbsences,
+            'failed' => $failedAbsences,
+        ], 'Absensi telah diproses', 201);
     }
+
 
 }
