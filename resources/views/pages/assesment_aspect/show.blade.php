8ff5e378461b8615dd754fbdf7f6cea36d4b7290
cintarmynti <ramayanticinta@gmail.com>
2025-04-23 15:16:06 +0700

    Update show.blade.php

--- resources/views/pages/assesment_aspect/show.blade.php
+++ resources/views/pages/assesment_aspect/show.blade.php
@@ -23,22 +23,22 @@
         </div>
 
         <!-- Packages -->
-<div class="row mb-4">
-    <div class="col-12 mb-3">
-        <label for="package" class="form-label">Packages Terkait</label>
-        @if($categories->packages->count())
-            <ul class="list-group">
-                @foreach($categories->packages as $package)
-                    <li class="list-group-item">
-                        <strong>{{ $package->name }}</strong> — {{ $package->desc }}
-                    </li>
-                @endforeach
-            </ul>
-        @else
-            <p class="text-muted">Belum ada paket yang terkait.</p>
-        @endif
-    </div>
-</div>
+        <div class="row mb-4">
+            <div class="col-12 mb-3">
+                <label for="package" class="form-label">Packages Terkait</label>
+                @if($categories->packages->count())
+                    <ul class="list-group">
+                        @foreach($categories->packages as $package)
+                            <li class="list-group-item">
+                                <strong>{{ $package->name }}</strong> — {{ $package->desc }}
+                            </li>
+                        @endforeach
+                    </ul>
+                @else
+                    <p class="text-muted">Belum ada paket yang terkait.</p>
+                @endif
+            </div>
+        </div>
 
 
         <!-- Kontainer Aspek dan Deskripsi -->
