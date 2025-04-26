d708b740c42c75e1dceb4e1d36b1d16301ae9110
cintarmynti <ramayanticinta@gmail.com>
2025-04-26 14:33:08 +0700

    Update NotificationController.php

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\Notification;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class NotificationController extends Controller
 {
     public function getNotifications()
     {
-        $notifications = Notification::latest()->take(5)->get();
+        $notifications = Notification::where('user_id', Auth::user()->id)->latest()->get();
         return response()->json($notifications);
     }
 }
